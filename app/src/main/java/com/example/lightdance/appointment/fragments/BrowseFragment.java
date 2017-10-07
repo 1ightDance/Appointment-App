850dcae664a81e1790cfe19ac4f3896956d20e5d
LightDance <773617349@qq.com>
2017-10-07 23:32:20 +0800

    增加BrowseMsgBean 重命名AppointmentMessageAdapter为BrowseMsgAdapter 注释掉了BrowseFragment里多余的东西

--- app/src/main/java/com/example/lightdance/appointment/fragments/BrowseFragment.java
+++ app/src/main/java/com/example/lightdance/appointment/fragments/BrowseFragment.java
@@ -14,16 +14,16 @@ import com.example.lightdance.appointment.R;
  */
 public class BrowseFragment extends Fragment {
 
-    //暂未考虑限制条件
-    private String title;
-    private String publishTime;
-    private String beginTime;
-    private String endTime;
-    private String place;
-    private String inviter;
-    private String type;
-    private int personNumber;
-    private int inviterIconId;
+//    //暂未考虑限制条件
+//    private String title;
+//    private String publishTime;
+//    private String beginTime;
+//    private String endTime;
+//    private String place;
+//    private String inviter;
+//    private String type;
+//    private int personNumber;
+//    private int inviterIconId;
 
 
     public BrowseFragment() {
@@ -55,42 +55,42 @@ public class BrowseFragment extends Fragment {
 //        this.personNumber = personNumber;
 //        this.inviterIconId = inviterIconId;
 //    }
-
-    //各字段get方法
-    public String getTitle() {
-        return title;
-    }
-
-    public String getPublishTime() {
-        return publishTime;
-    }
-
-    public String getBeginTime() {
-        return beginTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public String getPlace() {
-        return place;
-    }
-
-    public String getInviter() {
-        return inviter;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public int getPersonNumber() {
-        return personNumber;
-    }
-
-    public int getInviterIconId() {
-        return inviterIconId;
-    }
-
+//
+//    //各字段get方法
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public String getPublishTime() {
+//        return publishTime;
+//    }
+//
+//    public String getBeginTime() {
+//        return beginTime;
+//    }
+//
+//    public String getEndTime() {
+//        return endTime;
+//    }
+//
+//    public String getPlace() {
+//        return place;
+//    }
+//
+//    public String getInviter() {
+//        return inviter;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public int getPersonNumber() {
+//        return personNumber;
+//    }
+//
+//    public int getInviterIconId() {
+//        return inviterIconId;
+//    }
+//
 }
