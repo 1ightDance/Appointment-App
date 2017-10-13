2885eb11cd76ece8d058a75fd4688fdb5e510973
LightDance <773617349@qq.com>
2017-10-13 18:28:05 +0800

    新建了一个碎片打算用来写公开的用户简介

---
+++ app/src/main/java/com/example/lightdance/appointment/fragments/BrieflyIntroduceFragment.java
@@ -0,0 +1,32 @@
+package com.example.lightdance.appointment.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lightdance.appointment.R;
+
+
+public class BrieflyIntroduceFragment extends Fragment {
+
+    public BrieflyIntroduceFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_briefly_introduce, container, false);
+    }
+}
