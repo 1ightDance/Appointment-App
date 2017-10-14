9676bc366409b5276bf25fc2e38d59b912a5e21b
DESKTOP-TM6SFFS\pope <1173038073@qq.com>
2017-10-14 15:53:31 +0800

    漏发两个

--- app/src/main/java/com/example/lightdance/appointment/adapters/BrowserMsgAdapter.java
+++ app/src/main/java/com/example/lightdance/appointment/adapters/BrowserMsgAdapter.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.lightdance.appointment.Model.BrowseMsgBean;
 import com.example.lightdance.appointment.R;
 
 import java.util.List;
--- app/src/main/java/com/example/lightdance/appointment/fragments/BrowseFragment.java
+++ app/src/main/java/com/example/lightdance/appointment/fragments/BrowseFragment.java
@@ -3,28 +3,25 @@ package com.example.lightdance.appointment.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.lightdance.appointment.Model.BrowseMsgBean;
 import com.example.lightdance.appointment.R;
+import com.example.lightdance.appointment.adapters.BrowserMsgAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BrowseFragment extends Fragment {
-
-//    //暂未考虑限制条件
-//    private String title;
-//    private String publishTime;
-//    private String beginTime;
-//    private String endTime;
-//    private String place;
-//    private String inviter;
-//    private String type;
-//    private int personNumber;
-//    private int inviterIconId;
-
+    
+    private List<BrowseMsgBean> browseMsgBeen = new ArrayList<>();
 
     public BrowseFragment() {
         // Required empty public constructor
@@ -36,61 +33,20 @@ public class BrowseFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_browse, container, false);
+
+        initAppointmentMsg();
+        
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview_browse);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(
+                getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        BrowserMsgAdapter adapter = new BrowserMsgAdapter(browseMsgBeen);
+        recyclerView.setAdapter(adapter);
+
+
         return view;
     }
 
-
-
-    //构造方法
-//    public BrowseFragment(String title, String publishTime, String beginTime,
-//                                  String endTime, String place, String inviter, String type,
-//                                  int personNumber, int inviterIconId) {
-//        this.title = title;
-//        this.publishTime = publishTime;
-//        this.beginTime = beginTime;
-//        this.endTime = endTime;
-//        this.place = place;
-//        this.inviter = inviter;
-//        this.type = type;
-//        this.personNumber = personNumber;
-//        this.inviterIconId = inviterIconId;
-//    }
-//
-//    //各字段get方法
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public String getPublishTime() {
-//        return publishTime;
-//    }
-//
-//    public String getBeginTime() {
-//        return beginTime;
-//    }
-//
-//    public String getEndTime() {
-//        return endTime;
-//    }
-//
-//    public String getPlace() {
-//        return place;
-//    }
-//
-//    public String getInviter() {
-//        return inviter;
-//    }
-//
-//    public String getType() {
-//        return type;
-//    }
-//
-//    public int getPersonNumber() {
-//        return personNumber;
-//    }
-//
-//    public int getInviterIconId() {
-//        return inviterIconId;
-//    }
-//
+    private void initAppointmentMsg() {
+    }
 }
