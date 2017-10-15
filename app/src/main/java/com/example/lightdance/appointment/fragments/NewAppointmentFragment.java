4142f760d00d1e94cc001697242581fe79d985af
DESKTOP-TM6SFFS\pope <1173038073@qq.com>
2017-10-15 22:44:46 +0800

    完善逻辑 发布约人信息后自动跳转至广场碎片

--- app/src/main/java/com/example/lightdance/appointment/fragments/NewAppointmentFragment.java
+++ app/src/main/java/com/example/lightdance/appointment/fragments/NewAppointmentFragment.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.example.lightdance.appointment.Model.BrowseMsgBean;
 import com.example.lightdance.appointment.R;
+import com.example.lightdance.appointment.activities.MainActivity;
 
 import org.litepal.tablemanager.Connector;
 
@@ -128,6 +129,9 @@ public class NewAppointmentFragment extends Fragment {
             case R.id.new_appointment_done:
                 saveTypeData();
                 Toast.makeText(getActivity(), "约人信息发布成功", Toast.LENGTH_SHORT).show();
+                //发布成功后跳转至广场碎片
+                MainActivity activity = (MainActivity) getActivity();
+                activity.changeFragment(1);
                 break;
         }
     }
