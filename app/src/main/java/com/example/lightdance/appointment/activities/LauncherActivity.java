755281883b26ee914989198707416d746e2081ed
DESKTOP-TM6SFFS\pope <1173038073@qq.com>
2017-10-16 20:47:04 +0800

    新增启动页

---
+++ app/src/main/java/com/example/lightdance/appointment/activities/LauncherActivity.java
@@ -0,0 +1,31 @@
+package com.example.lightdance.appointment.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN , WindowManager.LayoutParams. FLAG_FULLSCREEN);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        Integer time = 3000;    //设置等待时间，单位为毫秒
+        Handler handler = new Handler();
+        //当计时结束时，跳转至主界面
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LauncherActivity.this, MainActivity.class));
+                LauncherActivity.this.finish();
+            }
+        }, time);
+    }
+}
