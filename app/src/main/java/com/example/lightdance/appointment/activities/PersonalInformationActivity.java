e1b2e9b2b146b0e0522b042d29abd62fb2a05ced
DESKTOP-TM6SFFS\pope <1173038073@qq.com>
2017-11-06 17:19:15 +0800

    新增个人资料部分 昵称和学院可修改且已接入后台

--- app/src/main/java/com/example/lightdance/appointment/activities/PersonalInformationActivity.java
+++ app/src/main/java/com/example/lightdance/appointment/activities/PersonalInformationActivity.java
@@ -1,5 +1,7 @@
 package com.example.lightdance.appointment.activities;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -9,12 +11,21 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.lightdance.appointment.Model.UserBean;
 import com.example.lightdance.appointment.R;
+import com.example.lightdance.appointment.fragments.SetPersonalInformationFragment;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
+import cn.bmob.v3.listener.UpdateListener;
 
 public class PersonalInformationActivity extends AppCompatActivity {
 
@@ -35,6 +46,12 @@ public class PersonalInformationActivity extends AppCompatActivity {
     TextView tvUserinforCollege;
     @BindView(R.id.tv_userinfor_studentnumb)
     TextView tvUserinforStudentnumb;
+
+    private SetPersonalInformationFragment editDialog = new SetPersonalInformationFragment();
+    private int setCode = 0;
+    private int SET_NICKNAME = 1;
+    private int SET_COLLEGE = 2;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,17 +89,101 @@ public class PersonalInformationActivity extends AppCompatActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.tv_userinfor_nickname:
+                editDialog.show(getSupportFragmentManager(),"editDialog");
+                setCode = SET_NICKNAME;
                 break;
             case R.id.img_userinfor_avatar:
+                Toast.makeText(this,"正在解锁ing...",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.tv_userinfor_name:
+                Toast.makeText(this,"姓名无法编辑",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.tv_userinfor_sex:
+                Toast.makeText(this,"性别无法编辑",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.tv_userinfor_college:
+                editDialog.show(getSupportFragmentManager(),"editDialog");
+                setCode = SET_COLLEGE;
                 break;
             case R.id.tv_userinfor_studentnumb:
+                Toast.makeText(this,"学号无法编辑",Toast.LENGTH_SHORT).show();
                 break;
         }
     }
+
+    public void setMsg(String s){
+        if (setCode == SET_NICKNAME){
+            setNickName(s);
+        }
+        if (setCode == SET_COLLEGE){
+            setCollege(s);
+        }
+    }
+
+    public void setNickName(String s){
+        tvUserinforNickname.setText(s);
+    }
+
+    public void setCollege(String s){
+        tvUserinforCollege.setText(s);
+    }
+
+    public void updateData(final String s){
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("请稍等");
+        progressDialog.setMessage("正在保存...");
+        progressDialog.setCancelable(false);
+        SharedPreferences p = getSharedPreferences("loginData", Context.MODE_PRIVATE);
+        BmobQuery<UserBean> query = new BmobQuery<>();
+        query.addWhereEqualTo("userStudentNum",p.getString("userStudentNumber","错误"));
+        query.findObjects(new FindListener<UserBean>() {
+            @Override
+            public void done(List<UserBean> list, BmobException e) {
+                if (e == null){
+                    SharedPreferences.Editor editor =getSharedPreferences("loginData",MODE_PRIVATE).edit();
+                    String objectId = list.get(0).getObjectId();
+                    UserBean user = new UserBean();
+                    if (setCode == SET_NICKNAME){
+                        editor.putString("nickName",s);
+                        editor.apply();
+                        user.setUserNickName(s);
+                        user.update(objectId, new UpdateListener() {
+                            @Override
+                            public void done(BmobException e) {
+                                if (e == null){
+                                    Toast.makeText(PersonalInformationActivity.this,"保存成功",Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(PersonalInformationActivity.this,"保存失败"+e.getMessage()
+                                            ,Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+//                        list.get(0).setValue("userNickName",s);
+                        progressDialog.dismiss();
+                    }
+                    if (setCode == SET_COLLEGE){
+                        editor.putString("userCollege",s);
+                        editor.apply();
+                        user.setUserCollege(s);
+                        user.update(objectId, new UpdateListener() {
+                            @Override
+                            public void done(BmobException e) {
+                                if (e == null){
+                                    Toast.makeText(PersonalInformationActivity.this,"保存成功",Toast.LENGTH_SHORT).show();
+                                }else {
+                                    Toast.makeText(PersonalInformationActivity.this, "保存失败" + e.getMessage()
+                                            , Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+//                        list.get(0).setValue("userCollege",s);
+                        progressDialog.dismiss();
+                    }
+                }else{
+                    Toast.makeText(PersonalInformationActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/lightdance/appointment/fragments/SetPersonalInformationFragment.java
@@ -0,0 +1,64 @@
+package com.example.lightdance.appointment.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.lightdance.appointment.R;
+import com.example.lightdance.appointment.activities.PersonalInformationActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SetPersonalInformationFragment extends DialogFragment {
+
+
+    @BindView(R.id.editText)
+    EditText editText;
+    @BindView(R.id.tv_edit_title)
+    TextView tvEditTitle;
+    Unbinder unbinder;
+
+    public SetPersonalInformationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_set_personal_information, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.img_edit_done)
+    public void onViewClicked() {
+        String s = editText.getText().toString();
+        PersonalInformationActivity a = (PersonalInformationActivity) getActivity();
+        a.setMsg(s);
+        a.updateData(s);
+        editText.setText("");
+        dismiss();
+    }
+
+}
