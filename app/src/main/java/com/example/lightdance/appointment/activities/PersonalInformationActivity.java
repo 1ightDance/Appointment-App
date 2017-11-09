6e44269720288265e6702156da9273aa651ef547
pope <1173038073@qq.com>
2017-11-09 17:06:06 +0800

    新增本地选头像

--- app/src/main/java/com/example/lightdance/appointment/activities/PersonalInformationActivity.java
+++ app/src/main/java/com/example/lightdance/appointment/activities/PersonalInformationActivity.java
@@ -2,11 +2,15 @@ package com.example.lightdance.appointment.activities;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -52,6 +56,10 @@ public class PersonalInformationActivity extends AppCompatActivity {
     private int SET_NICKNAME = 1;
     private int SET_COLLEGE = 2;
     private ProgressDialog progressDialog;
+    protected static final int CHOOSE_PICTURE = 0;
+    protected static Uri tempUri;
+    private static final int CROP_SMALL_PICTURE = 2;
+    private Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,7 +101,11 @@ public class PersonalInformationActivity extends AppCompatActivity {
                 setCode = SET_NICKNAME;
                 break;
             case R.id.img_userinfor_avatar:
-                Toast.makeText(this,"正在解锁ing...",Toast.LENGTH_SHORT).show();
+                Intent openAlbumIntent = new Intent(
+                        Intent.ACTION_GET_CONTENT);
+                openAlbumIntent.setType("image/*");
+                //用startActivityForResult方法，待会儿重写onActivityResult()方法，拿到图片做裁剪操作
+                startActivityForResult(openAlbumIntent, CHOOSE_PICTURE);
                 break;
             case R.id.tv_userinfor_name:
                 Toast.makeText(this,"姓名无法编辑",Toast.LENGTH_SHORT).show();
@@ -158,7 +170,6 @@ public class PersonalInformationActivity extends AppCompatActivity {
                                 }
                             }
                         });
-//                        list.get(0).setValue("userNickName",s);
                         progressDialog.dismiss();
                     }
                     if (setCode == SET_COLLEGE){
@@ -176,7 +187,6 @@ public class PersonalInformationActivity extends AppCompatActivity {
                                 }
                             }
                         });
-//                        list.get(0).setValue("userCollege",s);
                         progressDialog.dismiss();
                     }
                 }else{
@@ -184,6 +194,58 @@ public class PersonalInformationActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == MainActivity.RESULT_OK) {
+            switch (requestCode) {
+                case CHOOSE_PICTURE:
+                    cutImage(data.getData()); // 对图片进行裁剪处理
+                    break;
+                case CROP_SMALL_PICTURE:
+                    if (data != null) {
+                        setImageToView(data); // 让刚才选择裁剪得到的图片显示在界面上
+                    }
+                    break;
+            }
+        }
+    }
+
+    /**
+     * 裁剪图片方法实现
+     */
+    protected void cutImage(Uri uri) {
+        if (uri == null) {
+            Log.i("alanjet", "The uri is not exist.");
+        }
+        tempUri = uri;
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        //com.android.camera.action.CROP这个action是用来裁剪图片用的
+        intent.setDataAndType(uri, "image/*");
+        // 设置裁剪
+        intent.putExtra("crop", "true");
+        // aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, CROP_SMALL_PICTURE);
+    }
+    /**
+     * 保存裁剪之后的图片数据
+     */
+    protected void setImageToView(Intent data) {
+        Bundle extras = data.getExtras();
+        if (extras != null) {
+            mBitmap = extras.getParcelable("data");
+            //这里图片是方形的，可以用一个工具类处理成圆形（很多头像都是圆形，这种工具类网上很多不再详述）
+            imgUserinforAvatar.setImageBitmap(mBitmap);//显示图片
+            //在这个地方可以写上上传该图片到服务器的代码，后期将单独写一篇这方面的博客，敬请期待...
+        }
     }
 
 }
