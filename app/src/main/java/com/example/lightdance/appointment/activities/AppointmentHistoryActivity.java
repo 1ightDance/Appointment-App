86a5a341cffa17811268f04cd594368aa07a44a7
LightDance <773617349@qq.com>
2017-11-25 22:26:17 +0800

    完善了一部分的历史记录功能，顺便在几个有switch-case加了default规范一下

--- app/src/main/java/com/example/lightdance/appointment/activities/AppointmentHistoryActivity.java
+++ app/src/main/java/com/example/lightdance/appointment/activities/AppointmentHistoryActivity.java
@@ -15,14 +15,22 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lightdance.appointment.Model.BrowseMsgBean;
+import com.example.lightdance.appointment.Model.BrowserMsgBean;
 import com.example.lightdance.appointment.R;
+import com.example.lightdance.appointment.adapters.BrowserMsgAdapter;
 
 import org.litepal.crud.DataSupport;
 
 import java.util.List;
+
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
 
 public class AppointmentHistoryActivity extends AppCompatActivity {
 
@@ -56,7 +64,6 @@ public class AppointmentHistoryActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.history_container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
         TabLayout tabLayout = (TabLayout) findViewById(R.id.history_tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
@@ -87,7 +94,7 @@ public class AppointmentHistoryActivity extends AppCompatActivity {
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
         //用来取数据库数据
-        private List<BrowseMsgBean> msgHistoryList;
+        private BmobQuery<BrowserMsgBean> msgHistoryList;
         RecyclerView historyRecyclerView;
 
         public PlaceholderFragment() {
@@ -108,25 +115,66 @@ public class AppointmentHistoryActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_appointment_history, container, false);
+            //绑定视图
+            final View rootView = inflater.inflate(R.layout.fragment_appointment_history, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            //有点害怕这里设置完查询记录为空的textview之后不会消失
+            final TextView ifEmpty = (TextView) rootView.findViewById(R.id.history_if_empty);
             historyRecyclerView = (RecyclerView)rootView.findViewById(R.id.section_recyclerview);
             historyRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));SharedPreferences preferences = getActivity().getSharedPreferences("loginData",MODE_PRIVATE);
             String loginNickName = preferences.getString("nickName",null);
             String loginStudentId = preferences.getString("userStudentNumber",null);
             if (getArguments().getInt(ARG_SECTION_NUMBER) == 1){
-                //TODO 暂时这样代替，会改数据库取数据的逻辑
+                //TODO 暂时这样,希望之后能有个每次加载10行，下拉加载更多的逻辑,目前设置的只查50条是有隐患的                msgHistoryList = new BmobQuery<BrowserMsgBean>();
+                msgHistoryList.addWhereEqualTo("inviter" , loginNickName);
+                msgHistoryList.setLimit(50);
+                msgHistoryList.findObjects(new FindListener<BrowserMsgBean>() {
+                    @Override
+                    public void done(List<BrowserMsgBean> list, BmobException e) {
+                        if (e == null) {
+                            //查询成功，读取数据以供adapter保存
+                            Toast.makeText(getActivity(), "成功", Toast.LENGTH_SHORT);
+                            for (BrowserMsgBean msg : list) {
+                                msg.getContactWay();
+                                msg.getContent();
+                                msg.getEndTime();
+                                msg.getInviter();
+                                msg.getMembers();
+                                msg.getPersonNumberHave();
+                                msg.getPersonNumberNeed();
+                                msg.getPlace();
+                                msg.getStartTime();
+                                msg.getPublishTime();
+                                msg.getTitle();
+                                msg.getTypeCode();
+                            }
+                            BrowserMsgAdapter adapter = new BrowserMsgAdapter(getActivity(), list);
+                            historyRecyclerView.setAdapter(adapter);
+                            //如果为空，显示这样的字段
+                            if (list.isEmpty()) {
+                                ifEmpty.setText("发布记录空空如也");
+                            } else {
+                                //TODO 这里在考虑加幅网络连接失败图片啥的，但是不太会搞定那个抛异常
+                                Toast.makeText(getActivity(), "你网有毛病吧", Toast.LENGTH_SHORT);
+
+                            }
+                        }
+                    }
+                });
 //                msgHistoryList = DataSupport.where("inviter == ?",loginNickName).find(BrowseMsgBean.class);
 //                BrowserMsgAdapter adapter = new BrowserMsgAdapter(getActivity(),msgHistoryList);
 //                this.historyRecyclerView.setAdapter(adapter);
-                //如果为空，显示这样的字段
-                if (msgHistoryList.isEmpty()){
-                    textView.setText("发布记录空空如也");
-                }
-            }else if (getArguments().getInt(ARG_SECTION_NUMBER) == 2){
-                //TODO 几个用户id连在一起的话这样查绝逼有bug
-                msgHistoryList = DataSupport.where("participantsId like ?", "%" + loginStudentId + "%").find(BrowseMsgBean.class);
-                textView.setText("应约记录空空如也");
+                }else if (getArguments().getInt(ARG_SECTION_NUMBER) == 2){
+                //查询所有约帖中“应约者列表”，显示所有含有本账号的约帖 ...
+                //TODO  我觉着会非常慢，效率感人，需要改进一下...而且setLimit存在同上隐患
+                msgHistoryList = msgHistoryList = new BmobQuery<BrowserMsgBean>();
+                msgHistoryList.addWhereEqualTo("inviter" , loginNickName);
+                msgHistoryList.setLimit(50);
+                msgHistoryList.findObjects(new FindListener<BrowserMsgBean>() {
+                    @Override
+                    public void done(List<BrowserMsgBean> list, BmobException e) {
+                    }
+                });
             }
             return rootView;
         }
@@ -162,6 +210,7 @@ public class AppointmentHistoryActivity extends AppCompatActivity {
                     return "发布";
                 case 1:
                     return "应约";
+                    default:
             }
             return null;
         }
