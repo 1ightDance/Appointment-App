a1e4b974f5ded1a143ceb15c4b40d1d9d1a0101b
pope <1173038073@qq.com>
2017-12-10 15:34:22 +0800

    添加检索HistoryBean中ongoingAppointment中的活动方法，若该活动已结束将该活动从正在进行中的活动列表移到已结束活动列表

--- app/src/main/java/com/example/lightdance/appointment/activities/LauncherActivity.java
+++ app/src/main/java/com/example/lightdance/appointment/activities/LauncherActivity.java
@@ -7,20 +7,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 
+/**
+ * @author pope
+ */
 public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN , WindowManager.LayoutParams. FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
-        Integer time = 500;    //设置等待时间，单位为毫秒
+        //设置等待时间，单位为毫秒
+        Integer time = 500;
         Handler handler = new Handler();
+
         //当计时结束时，跳转至主界面
         handler.postDelayed(new Runnable() {
+
             @Override
             public void run() {
                 startActivity(new Intent(LauncherActivity.this, MainActivity.class));
--- app/src/main/java/com/example/lightdance/appointment/activities/MainActivity.java
+++ app/src/main/java/com/example/lightdance/appointment/activities/MainActivity.java
@@ -11,10 +11,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.lightdance.appointment.Model.BrowserMsgBean;
 import com.example.lightdance.appointment.Model.HistoryBean;
 import com.example.lightdance.appointment.R;
 import com.example.lightdance.appointment.fragments.MessageFragment;
@@ -23,6 +25,8 @@ import com.example.lightdance.appointment.fragments.PersonalCenterFragment;
 import com.example.lightdance.appointment.fragments.TypeFragment;
 import com.example.lightdance.appointment.fragments.WarningFragment;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import cn.bmob.sms.BmobSMS;
@@ -30,6 +34,8 @@ import cn.bmob.v3.Bmob;
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.FindListener;
+import cn.bmob.v3.listener.QueryListener;
+import cn.bmob.v3.listener.UpdateListener;
 
 /**
  * @author lightdance
@@ -57,6 +63,15 @@ public class MainActivity extends AppCompatActivity {
         //初始化Bmob SDK
         Bmob.initialize(this, "7420a33e6758604ec1e823f1378f4e61");
         BmobSMS.initialize(this, "7420a33e6758604ec1e823f1378f4e61");
+
+        //检测当前用户是否登录 若登录 进行一些数据检查操作
+        SharedPreferences sharedPreferences = getSharedPreferences("loginData", MODE_PRIVATE);
+        boolean isLogined = sharedPreferences.getBoolean("isLogined", false);
+        if (isLogined) {
+            String userObjectId = sharedPreferences.getString("userBeanId", "出错啦~");
+            //遍历检查正在进行的活动是否已结束
+            checkOngoingAppointment(userObjectId);
+        }
 
         //检查是否有未完成反馈的已结束活动
         checkIsComment();
@@ -89,6 +104,85 @@ public class MainActivity extends AppCompatActivity {
 
         changeFragment(1);
 
+    }
+
+    public void checkOngoingAppointment(String userObjectId) {
+
+        final Calendar cal;
+        int year, month, day, hour, min;
+
+        //获取当前时间 并换算为毫秒
+        cal = Calendar.getInstance();
+        year = cal.get(Calendar.YEAR);
+        month = cal.get(Calendar.MONTH) + 1;
+        day = cal.get(Calendar.DAY_OF_MONTH);
+        hour = cal.get(Calendar.HOUR_OF_DAY);
+        min = cal.get(Calendar.MINUTE);
+        cal.set(year, month, day, hour, min);
+        final long sysMillisSec = cal.getTimeInMillis();
+
+        BmobQuery<HistoryBean> q1 = new BmobQuery<>();
+        q1.addWhereEqualTo("userObjectId", userObjectId);
+        q1.findObjects(new FindListener<HistoryBean>() {
+            @Override
+            public void done(final List<HistoryBean> list, BmobException e) {
+                if (e == null) {
+                    if (list.size() != 0) {
+                        final HistoryBean historyBean = list.get(0);
+                        List<String> ongoingList = historyBean.getOngoingAppointment();
+                        for (int i = 0; i < ongoingList.size(); i++) {
+                            final String browserObjectId = ongoingList.get(i);
+                            BmobQuery<BrowserMsgBean> q2 = new BmobQuery<>();
+                            final int finalI = i;
+                            q2.getObject(browserObjectId, new QueryListener<BrowserMsgBean>() {
+                                @Override
+                                public void done(BrowserMsgBean browserMsgBean, BmobException e) {
+                                    if (e == null) {
+                                        Log.i("调试","第"+ finalI +"次查询");
+                                        String endTime = browserMsgBean.getEndTime();
+                                        int year = Integer.valueOf(endTime.substring(0, 4));
+                                        int month = Integer.valueOf(endTime.substring(5, 7));
+                                        int day = Integer.valueOf(endTime.substring(8, 10));
+                                        int hour = Integer.valueOf(endTime.substring(12, 14));
+                                        int min = Integer.valueOf(endTime.substring(15, 17));
+                                        cal.set(year, month, day, hour, min);
+                                        long endMillisSec = cal.getTimeInMillis();
+                                        if (sysMillisSec >= endMillisSec) {
+                                            List<String> ongoingList = historyBean.getOngoingAppointment();
+                                            List<String> finishedList = historyBean.getFinishedAppointment();
+                                            for (int i = ongoingList.size() - 1; i >= 0; i--) {
+                                                String item = ongoingList.get(i);
+                                                if (browserObjectId.equals(item)) {
+                                                    ongoingList.remove(item);
+                                                }
+                                            }
+                                            if (finishedList == null){
+                                                finishedList = new ArrayList<>();
+                                            }
+                                            finishedList.add(browserObjectId);
+                                            historyBean.setValue("ongoingAppointment", ongoingList);
+                                            historyBean.setValue("finishedAppointment", finishedList);
+                                            historyBean.update(historyBean.getObjectId(), new UpdateListener() {
+                                                @Override
+                                                public void done(BmobException e) {
+                                                    if (e != null) {
+                                                        Log.i("调试","遍历未结束活动数据后更改数据存储出错" + e.getMessage());
+                                                    }
+                                                }
+                                            });
+                                        }
+                                    } else {
+                                        Log.i("调试","遍历未结束活动数据时出错" + e.getMessage());
+                                    }
+                                }
+                            });
+                        }
+                    }
+                } else {
+                    Log.i("调试","查询当前用户的历史表数据出错" + e.getMessage());
+                }
+            }
+        });
     }
 
     private void checkIsComment() {
